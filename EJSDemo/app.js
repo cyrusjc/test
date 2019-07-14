9e7672e8930e69998846b78a8de8d36a73b92a27
EC2 Default User <ec2-user@ip-172-31-17-206.us-west-2.compute.internal>
2019-07-14 19:24:18 +0000

    pushing module

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -1,12 +1,23 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+
+var friends = ["bob", "swag"];
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
-    res.render("home.ejs")
+    res.render("home.ejs", {friends: friends})
 })
+
+app.post("/addfriend", function(req,res){
+//PUSHING THE POST REQUEST INTO THE ARRAY!
+   friends.push(req.body.newfriend);
+   res.redirect("/");
+});
 
 app.get("/fallinlovewith/:thing", function(req,res){
     var thing = req.params.thing
